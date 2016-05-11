96c168bd44627107c8b3464455e857c632135be7
matteo <matteo.ghetta@gmail.com>
2016-05-11 17:12:17 +0200

    data defined button for marker size

--- plots/scatter_dialog.py
+++ plots/scatter_dialog.py
@@ -28,6 +28,8 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.QtWebKit import QWebView
 from qgis.gui import *
+from qgis.core import QgsExpression, QgsVectorLayer
+import matplotlib.colors as colors
 import plotly
 import plotly.graph_objs as go
 
@@ -46,36 +48,63 @@ class ScatterPlotDialog(QtGui.QDialog, FORM_CLASS):
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
         self.buttonBox.button(QDialogButtonBox.Ok).clicked.connect(self.Scatter)
+        # method to connect the changing layer to data defined button
+        self.layerCombo.layerChanged.connect(self.updateData)
+
+    # slot of data defined receive signal of MapLayerComboBox
+    @pyqtSlot(QgsVectorLayer)
+    def updateData(self, layer):
+        self.dataDefined.init(layer)
 
 
     def Scatter(self):
 
         # get layer and the selected fields (signals and update directly in the UI)
-        lay1 = self.Field1.layer()
-        lay1_f = self.Field1.currentField()
-        lay2 = self.Field2.layer()
-        lay2_f = self.Field2.currentField()
+        lay1 = self.expField1.layer()
+        lay1_f = self.expField1.currentText()
+        lay2 = self.expField2.layer()
+        lay2_f = self.expField2.currentText()
 
         # layer for the point size
-        lay3 = self.Field3.layer()
-        lay3_f = self.Field3.currentField()
-
-
+        # lay3 = self.Field3.layer()
+        # lay3_f = self.Field3.currentField()
 
 
         # build the lists from the selected fields
         f1 = []
-        for i in lay1.getFeatures():
-            f1.append(i[lay1_f])
-
         f2 = []
-        for i in lay2.getFeatures():
-            f2.append(i[lay2_f])
+
+        # cicle to use normal field or selected expression for first layer
+        if self.expField1.currentField()[1] == False:
+            for i in lay1.getFeatures():
+                f1.append(i[lay1_f])
+        else:
+            filter1 = self.expField1.currentField()[0]
+            exp1 = QgsExpression(filter1)
+            for i in lay1.getFeatures():
+                f1.append(exp1.evaluate(i, lay1.pendingFields()))
+
+        # cicle to use normal field or selected expression for second layer
+        if self.expField2.currentField()[1] == False:
+            for i in lay2.getFeatures():
+                f2.append(i[lay2_f])
+        else:
+            filter2 = self.expField2.currentField()[0]
+            exp2 = QgsExpression(filter2)
+            for i in lay2.getFeatures():
+                f2.append(exp2.evaluate(i, lay2.pendingFields()))
 
 
-        # f3 = []
-        # for i in lay3.getFeatures():
-        #     f3.append(i[lay3_f])
+        # get the color button and the hex raw color code of the selected color
+        colbutton = self.colorButton
+        colorhex = colbutton.color().name()
+
+        # convert the hex color code to rgb code
+        colorrgb = colors.hex2color(colorhex)
+
+
+        # value of the slider for the alpha channel
+        alphavalue = self.alpha.value()
 
         # legend checkbox (default is checked = True)
         if self.legendCheck.isChecked():
@@ -83,7 +112,38 @@ class ScatterPlotDialog(QtGui.QDialog, FORM_CLASS):
         else:
             legend = False
 
-        S = self.Size.value()
+        # size settings
+
+        if self.dataDefined.isActive() == False:
+            markSize = self.Size.value()
+        else:
+            markSize = []
+            if self.dataDefined.useExpression() == True:
+                sizefilter = self.dataDefined.getExpression()
+                sizeexp = QgsExpression(sizefilter)
+                for i in self.layerCombo.currentLayer().getFeatures():
+                    markSize.append(sizeexp.evaluate(i, self.layerCombo.currentLayer().pendingFields()))
+            else:
+                for i in self.layerCombo.currentLayer().getFeatures():
+                    markSize.append(i[self.dataDefined.getField()])
+
+
+        print 'markerrrrrrrrrrr'
+        print markSize
+
+
+
+        # trying to set datadefined button
+
+        print 'rida campo'
+        print self.dataDefined.getField()
+        print self.dataDefined.getExpression()
+
+        print 'attivo'
+        print self.dataDefined.isActive()
+
+        print 'espressione o campo'
+        print self.dataDefined.useExpression()
 
 
         # initialize the Bar plot with the first trace
@@ -92,8 +152,9 @@ class ScatterPlotDialog(QtGui.QDialog, FORM_CLASS):
         y = f2,
         mode = 'markers',
         name = 'nome in legenda',
-        marker = dict(
-        size=30
+        marker = dict(color = 'rgb' + str(colorrgb),
+        size = markSize,
+        opacity = (100 - alphavalue) / 100.0
         )
         )
 
