438bf3bba86a2126fdc77801548c356a84cba920
matteo <matteo.ghetta@gmail.com>
2016-05-11 17:14:47 +0200

    resources recompilation

--- resources.py
+++ resources.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Mon May 9 17:07:21 2016
+# Created: Wed May 11 15:27:30 2016
 #      by: The Resource Compiler for PyQt (Qt v4.8.6)
 #
 # WARNING! All changes made in this file will be lost!
